b96f99f46a89f029b95abd8eef03f5ffcb620fac
PhamThiThanhDuyen <duyenptt97@gmail.com>
2025-08-21 23:32:44 +0700

    add chrome test

--- src/test/java/browsers/ChromeTest.java
+++ src/test/java/browsers/ChromeTest.java
@@ -7,14 +7,14 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ChromeTest {
-    @Test
-    public void normalModeTest(){
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.selenium.dev/");
-        Assert.assertEquals(driver.getTitle(),"Selenium");
-
-        driver.quit();
-    }
+//    @Test
+//    public void normalModeTest(){
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("https://www.selenium.dev/");
+//        Assert.assertEquals(driver.getTitle(),"Selenium");
+//
+//        driver.quit();
+//    }
 
     @Test
     public void headlessModeTest(){
